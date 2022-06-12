8242c42d014a99c7719146287860fc69ae54cad4
bravo567 <bravo567@bravo>
2022-06-12 16:50:56 +0800

    Initial Commit

---
+++ TomatoFasting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TomatoFasting
+//
+//  Created by heliang on 2022/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TomatoFasting/TomatoFastingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomatoFastingApp.swift
+//  TomatoFasting
+//
+//  Created by heliang on 2022/6/12.
+//
+
+import SwiftUI
+
+@main
+struct TomatoFastingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
